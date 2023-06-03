b448d600f9a441b23330f79b2602ec3443b94e21
DominikHomer <d.homer@student.tugraz.at>
2023-06-03 11:31:05 +0200

    added remaining sounds and implemented them

--- Assets/SFX/SFXPlayer.cs
+++ Assets/SFX/SFXPlayer.cs
@@ -7,6 +7,9 @@ public class SFXPlayer : MonoBehaviour
     public AudioSource bomb;
     public AudioSource game_start;
     public AudioSource button_click;
+    public AudioSource you_won;
+    public AudioSource step;
+    public AudioSource you_died;
 
     public void PlayBomb()
     {
@@ -21,5 +24,25 @@ public class SFXPlayer : MonoBehaviour
     public void Click()
     {
         button_click.Play();
+    }
+
+    public void YouWon()
+    {
+        you_won.Play();
+    }
+
+    public void YouDied()
+    {
+        you_died.Play();
+    }
+
+    public void Step()
+    {
+        step.Play();
+    }
+
+    public void StopStep()
+    {
+        step.Stop();
     }
 }
